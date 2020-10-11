7375e788224904dc449b6adb692e34a9b819ec0c
joao <you@example.com>
2020-10-11 11:50:33 -0300

    inicio do curso

---
+++ Exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,11 @@
+//O objetivo aqui é entender a linguagem em sua forma mais basilar
+//JavaScripit é formado por sentenças de códigos
+console.log("Sentença de código")
+
+//também é organizado em blocos de código
+{
+    {
+        console.log("Olá")
+        console.log('Mundo!')
+    }
+}
